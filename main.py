864f0a55528a95efdf7fac8be266224c2ca0e376
stern.za <stern.za@husky.neu.edu>
2019-11-17 20:45:44 -0500

    some stuff is on the screen

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from visualizer import *
 
 app = QtGui.QApplication(sys.argv)
 
+
 def record_qt_multimedia():
     info = QtMultimedia.QAudioDeviceInfo.defaultInputDevice()
     format = info.preferredFormat()
@@ -21,7 +22,7 @@ def record_qt_multimedia():
         print 'Format not supported, using nearest available'
         format = nearestFormat(format)
         if format.sampleSize != SAMPLE_SIZE:
-            #this is important, since effects assume this sample size.
+            # this is important, since effects assume this sample size.
             raise RuntimeError('16-bit sample size not supported!')
 
     audio_input = QtMultimedia.QAudioInput(format, app)
@@ -32,30 +33,37 @@ def record_qt_multimedia():
         data = np.fromstring(source.readAll(), 'int16').astype(float)
         if len(data):
             return data
+
     return read_data
+
 
 def record_pyaudio():
     p = pyaudio.PyAudio()
 
-    stream = p.open(format = pyaudio.paInt16,
-                    channels = CHANNEL_COUNT,
-                    rate = SAMPLE_RATE,
-                    input = True,
-                    frames_per_buffer = BUFFER_SIZE)
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=CHANNEL_COUNT,
+                    rate=SAMPLE_RATE,
+                    input=True,
+                    frames_per_buffer=BUFFER_SIZE)
 
     def read_data():
         data = np.fromstring(stream.read(stream.get_read_available()), 'int16').astype(float)
         if len(data):
             return data
+
     return read_data
+
+
 try:
     from PySide import QtMultimedia
+
     read_data = record_qt_multimedia()
 except ImportError:
     print 'Using PyAudio'
     import pyaudio
+
     read_data = record_pyaudio()
 
-window = LineVisualizer(read_data)
+window = CustomVisualizer(read_data)
 window.show()
 app.exec_()
--- visualizer.py
+++ visualizer.py
@@ -3,17 +3,19 @@
 import sys
 import random
 import time
+import math
 
 import numpy as np
 from PySide import QtCore, QtGui
+from PySide.QtCore import QPointF, Qt
 
 SAMPLE_MAX = 32767
 SAMPLE_MIN = -(SAMPLE_MAX + 1)
-SAMPLE_RATE = 44100 # [Hz]
+SAMPLE_RATE = 44100  # [Hz]
 NYQUIST = SAMPLE_RATE / 2
-SAMPLE_SIZE = 16 # [bit]
+SAMPLE_SIZE = 16  # [bit]
 CHANNEL_COUNT = 1
-BUFFER_SIZE = 5000 
+BUFFER_SIZE = 5000
 
 
 class Visualizer(QtGui.QLabel):
@@ -26,21 +28,21 @@ class Visualizer(QtGui.QLabel):
     Note: Although this is an abstract class, it cannot have a metaclass of
     abcmeta since it is a child of QObject.
     """
+
     def __init__(self, get_data, update_interval=33):
         super(Visualizer, self).__init__()
-        
+
         self.get_data = get_data
-        self.update_interval = update_interval #33ms ~= 30 fps
-        self.sizeHint = lambda: QtCore.QSize(400, 400)
+        self.update_interval = update_interval  # 33ms ~= 30 fps
+        self.sizeHint = lambda: QtCore.QSize(800, 800)
         self.setStyleSheet('background-color: black;');
         self.setWindowTitle('PyVisualizer')
-        
+
     def show(self):
         """Show the label and begin updating the visualization."""
         super(Visualizer, self).show()
         self.refresh()
-        
-        
+
     def refresh(self):
         """Generate a frame, display it, and set queue the next frame"""
         data = self.get_data()
@@ -48,19 +50,19 @@ class Visualizer(QtGui.QLabel):
         if data is not None:
             t1 = time.clock()
             self.setPixmap(QtGui.QPixmap.fromImage(self.generate(data)))
-            #decrease the time till next frame by the processing tmie so that the framerate stays consistent
+            # decrease the time till next frame by the processing tmie so that the framerate stays consistent
             interval -= 1000 * (time.clock() - t1)
         if self.isVisible():
             QtCore.QTimer.singleShot(self.update_interval, self.refresh)
-        
+
     def generate(self, data):
         """This is the abstract function that child classes will override to
         draw a frame of the visualization.
         
         The function takes an array of data and returns a QImage to display"""
         raise NotImplementedError()
-    
-    
+
+
 class LineVisualizer(Visualizer):
     """This visualizer will display equally sized rectangles
     alternating between black and another color, with the height of the
@@ -70,21 +72,21 @@ class LineVisualizer(Visualizer):
 
     def __init__(self, get_data, columns=1):
         super(LineVisualizer, self).__init__(get_data)
-        
+
         self.columns = columns
-        self.brushes = [QtGui.QBrush(QtGui.QColor(255, 255, 255)), #white
-                        QtGui.QBrush(QtGui.QColor(255, 0, 0)),     #red
-                        QtGui.QBrush(QtGui.QColor(0, 240, 0)),     #green
-                        QtGui.QBrush(QtGui.QColor(0, 0, 255)),     #blue
-                        QtGui.QBrush(QtGui.QColor(255, 255, 0)),   #yellow
-                        QtGui.QBrush(QtGui.QColor(0, 255, 255)),   #teal
+        self.brushes = [QtGui.QBrush(QtGui.QColor(255, 255, 255)),  # white
+                        QtGui.QBrush(QtGui.QColor(255, 0, 0)),  # red
+                        QtGui.QBrush(QtGui.QColor(0, 240, 0)),  # green
+                        QtGui.QBrush(QtGui.QColor(0, 0, 255)),  # blue
+                        QtGui.QBrush(QtGui.QColor(255, 255, 0)),  # yellow
+                        QtGui.QBrush(QtGui.QColor(0, 255, 255)),  # teal
                         ]
         self.brush = self.brushes[0]
-        
+
         self.display_odds = True
         self.display_evens = True
         self.is_fullscreen = False
-        
+
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_I:
             self.display_evens = True
@@ -104,7 +106,7 @@ class LineVisualizer(Visualizer):
                 self.showFullScreen()
                 self.is_fullscreen = True
         else:
-            #Qt.Key enum helpfully defines most keys as their ASCII code,
+            # Qt.Key enum helpfully defines most keys as their ASCII code,
             #   so we can use ord('Q') instead of Qt.Key.Key_Q
             color_bindings = dict(zip((ord(i) for i in 'QWERTYU'), self.brushes))
             try:
@@ -114,31 +116,30 @@ class LineVisualizer(Visualizer):
                     self.columns = 10
                 elif QtCore.Qt.Key_1 <= event.key() <= QtCore.Qt.Key_9:
                     self.columns = event.key() - QtCore.Qt.Key_1 + 1
-            
+
     def generate(self, data):
         fft = np.absolute(np.fft.rfft(data, n=len(data)))
-        freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
+        freq = np.fft.fftfreq(len(fft), d=1. / SAMPLE_RATE)
         max_freq = abs(freq[fft == np.amax(fft)][0])
         max_amplitude = np.amax(data)
-        
+
         rect_width = int(self.width() / (self.columns * 2))
-        
-        freq_cap = 20000. #this determines the scale of lines
+
+        freq_cap = 20000.  # this determines the scale of lines
         if max_freq >= freq_cap:
             rect_height = 1
         else:
             rect_height = int(self.height() * max_freq / freq_cap)
             if rect_height == 2: rect_height = 1
-            
-        
+
         img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
-        img.fill(0) #black
-        
+        img.fill(0)  # black
+
         if rect_height >= 1:
             painter = QtGui.QPainter(img)
             painter.setPen(QtCore.Qt.NoPen)
-            painter.setBrush(self.brush) 
-            
+            painter.setBrush(self.brush)
+
             for x in xrange(0, self.width() - rect_width, rect_width * 2):
                 for y in xrange(0, self.height(), 2 * rect_height):
                     if random.randint(0, int(max_amplitude / float(SAMPLE_MAX) * 10)):
@@ -146,41 +147,157 @@ class LineVisualizer(Visualizer):
                             painter.drawRect(x, y, rect_width, rect_height)
                         if self.display_odds:
                             painter.drawRect(x + rect_width, self.height() - y - rect_height, rect_width, rect_height)
-            
-            del painter #
-            
+
+            del painter  #
+
         return img
+
 
 class Spectrogram(Visualizer):
     def generate(self, data):
         fft = np.absolute(np.fft.rfft(data, n=len(data)))
-        freq = np.fft.fftfreq(len(fft), d=1./SAMPLE_RATE)
+        freq = np.fft.fftfreq(len(fft), d=1. / SAMPLE_RATE)
         max_freq = abs(freq[fft == np.amax(fft)][0]) / 2
         max_amplitude = np.amax(data)
-        
+
         bins = np.zeros(200)
-        #indices = (len(fft) - np.logspace(0, np.log10(len(fft)), len(bins), endpoint=False).astype(int))[::-1]
-        #for i in xrange(len(bins) - 1):
+        # indices = (len(fft) - np.logspace(0, np.log10(len(fft)), len(bins), endpoint=False).astype(int))[::-1]
+        # for i in xrange(len(bins) - 1):
         #    bins[i] = np.mean(fft[indices[i]:indices[i+1]]).astype(int)
-        #bins[-1] = np.mean(fft[indices[-1]:]).astype(int)
-        
+        # bins[-1] = np.mean(fft[indices[-1]:]).astype(int)
+
         step = int(len(fft) / len(bins))
         for i in xrange(len(bins)):
-            bins[i] = np.mean(fft[i:i+step])
-            
+            bins[i] = np.mean(fft[i:i + step])
+
         img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
         img.fill(0)
         painter = QtGui.QPainter(img)
         painter.setPen(QtCore.Qt.NoPen)
-        painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255))) #white)
-        
+        painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255)))  # white)
+
         for i, bin in enumerate(bins):
             height = self.height() * bin / float(SAMPLE_MAX) / 10
             width = self.width() / float(len(bins))
             painter.drawRect(i * width, self.height() - height, width, height)
-            
+
         del painter
-        
+
         return img
-        
-        
+
+
+class CustomVisualizer(Visualizer):
+    def __init__(self, get_data, columns=1):
+        super(CustomVisualizer, self).__init__(get_data)
+
+        self.columns = columns
+        self.brushes = [QtGui.QBrush(QtGui.QColor(255, 255, 255)),  # white
+                        QtGui.QBrush(QtGui.QColor(255, 0, 0)),  # red
+                        QtGui.QBrush(QtGui.QColor(0, 240, 0)),  # green
+                        QtGui.QBrush(QtGui.QColor(0, 0, 255)),  # blue
+                        QtGui.QBrush(QtGui.QColor(255, 255, 0)),  # yellow
+                        QtGui.QBrush(QtGui.QColor(0, 255, 255)),  # teal
+                        ]
+        self.brush = self.brushes[0]
+
+        # custom constants
+        self.ampl_range = (1000, 20000)  # (lower bound, spectrum width)
+        self.freq_step_size = 50 # 24 points maximum, 2 minimum
+        self.max_points = 24
+        self.freq_min = 200
+        self.inner_rad = 200
+        self.inner_circ = 2 * math.pi * self.inner_rad
+        self.outer_rad = 400
+        self.outer_circ = 2 * math.pi * self.outer_rad
+        self.center = (400, 400)
+        # end custom constants
+
+        self.display_odds = True
+        self.display_evens = True
+        self.is_fullscreen = False
+
+    def __getStarPoints__(self, points):
+        """
+
+        :param points: Int
+        :return: [Array-of QPointF]
+        """
+
+        star = QtGui.QPolygon()
+
+        for point in range(0, points):
+            point1, point2 = self.__getStarPair__(points, point)
+            star.append(point1)
+            star.append(point2)
+
+        return star
+
+    def __getStarPair__(self, points, idx):
+        """
+
+        :param points: int
+        :param idx: int
+        :return: QPointF, QPointF
+        """
+        inner_angle = float(idx) / points * 2 * math.pi
+        outer_angle = (float(idx) * 2 + 1) / points * 2 * math.pi
+        inner_x = self.inner_rad * math.cos(inner_angle) + self.center[0]
+        inner_y = self.inner_rad * math.sin(inner_angle) + self.center[1]
+        outer_x = self.outer_rad * math.cos(outer_angle) + self.center[0]
+        outer_y = self.outer_rad * math.sin(outer_angle) + self.center[1]
+
+        inner_point = QPointF(inner_x, inner_y)
+        outer_point = QPointF(outer_x, outer_y)
+
+        return inner_point, outer_point
+
+    def __getColor__(self, ampl):
+        """
+
+        :param ampl: Float
+        :return: QBrush
+        """
+        color = (ampl - self.ampl_range[0]) / self.ampl_range[1] * 255
+        if color < 0:
+            color = 0
+
+        if color > 255:
+            color = 255
+
+        brush = QtGui.QBrush(QtGui.QColor(color, 255 - color, 0))
+        return brush
+
+    def __getPoints__(self, max_freq):
+        points = (max_freq - self.freq_min) / self.freq_step_size
+
+        if points < 2:
+            points = 2
+
+        if points > self.max_points:
+            points = self.max_points
+
+        return self.__getStarPoints__(int(points))
+
+
+    def generate(self, data):
+        fft = np.absolute(np.fft.rfft(data, n=len(data)))
+        freq = np.fft.fftfreq(len(fft), d=1. / SAMPLE_RATE)
+        max_freq = abs(freq[fft == np.amax(fft)][0])
+        max_amplitude = np.amax(data)
+        brush = self.__getColor__(max_amplitude)
+        star_points = self.__getPoints__(max_freq)
+
+
+        # ----------------------------------------------------------------------------- #
+
+        img = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
+        img.fill(0)  # black
+
+        painter = QtGui.QPainter(img)
+        # painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(brush)
+
+        painter.drawPolyline(star_points)
+        del painter  #
+
+        return img
